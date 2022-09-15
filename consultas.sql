53876e038116d24700bda52e0f2d583bd0ea517d
joselio <milquiann@gmail.com>
2022-09-15 20:05:46 +0100

    actualizando archivos

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,7 @@
+
+# eliminamos las vistas si ya existen 
+drop view if exists pregunta1,pregunta2,pregunta3,pregunta4;
+
 -- Año con más carreras
 create view pregunta1 as  select  year , count(year) as cantidad   from  races  group by year order by  cantidad desc limit 1;
 
@@ -9,13 +13,32 @@ create view pregunta3 as  SELECT count(r.circuitId) as cantidad , c.name FROM ra
 
 
 -- Piloto con mayor cantidad de puntos en total, cuyo constructor sea de nacionalidad sea American o British
+
+
+select driverId,sum(points) as re from results  group by driverId order by driverId asc;
+select driverId,sum(points) as re from results  where driverId=154 ;
+
 create view pregunta4 as 
-select  d.name, sum(r.points) as puntos  
-from  results r
+(select r.driverId,sum(r.points) as sumapuntos , d.name , c.nationality
+from results r
 inner join  drivers d
 on  r.driverId=d.driverId
-group by r.driverId
-order by puntos desc limit 1;
+inner join constructor c 
+on r.constructorId=c.constructorId
+where c.nationality="American"
+group by r.driverId limit 1 )
+union all
+(select r.driverId,sum(r.points) as sumapuntos , d.name , c.nationality
+from results r
+inner join  drivers d
+on  r.driverId=d.driverId
+inner join constructor c 
+on r.constructorId=c.constructorId
+where c.nationality="British"
+group by r.driverId limit 1 );
+select * from pregunta4 ;
+select * from constructor where nationality="American" or nationality="British";
 
 
-drop view if exists pregunta1,pregunta2,pregunta3,pregunta4
+
+
