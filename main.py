9fe262025360d7161aec9fc2c58ade56b77d71f1
joselio <milquiann@gmail.com>
2022-09-16 22:50:57 +0100

    actualizando

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ def read_root():
 #se crea un funcuion para mostrar los datos de la primera pregunta " Año con más carreras "
 @app.get("/uno")
 def  uno():
-    miConexion = mysql.connector.connect( host="ace.com.pe",
-                                          user= 'acecompe_prueba1',
-                                          passwd='.&ve3Z5_VJW0', 
-                                          db="acecompe_prueba1"  )  
-                                         
+    miConexion = mysql.connector.connect( host="estacion.educatics.org",
+                                          user= 'educaics_usr_est',
+                                          passwd='F5z!xZ5jhSyg', 
+                                          db="educaics_db_estacion"  )  
     cur = miConexion.cursor()
     cur.execute("select * from pregunta1" )
     datos = [row for row in cur.fetchall()]
@@ -31,10 +30,10 @@ def  uno():
 #Piloto con mayor cantidad de primeros puestos
 @app.get("/dos")
 def  dos():
-    miConexion = mysql.connector.connect( host="ace.com.pe",
-                                          user= 'acecompe_prueba1',
-                                          passwd='.&ve3Z5_VJW0', 
-                                          db="acecompe_prueba1"  )     
+    miConexion = mysql.connector.connect( host="estacion.educatics.org",
+                                          user= 'educaics_usr_est',
+                                          passwd='F5z!xZ5jhSyg', 
+                                          db="educaics_db_estacion" )     
     cur = miConexion.cursor()
     cur.execute("SELECT MAX(gan.driver) as ' carreras ganadas', gan.driverId as piloto, d.name as Nombre FROM (SELECT driverId, COUNT(driverId) as driver FROM results WHERE positionOrder='1' GROUP BY driverId) gan JOIN drivers d  ON (gan.driverId=d.driverId);" )
     datos2 = [row for row in cur.fetchall()]
@@ -45,10 +44,11 @@ def  dos():
 #Nombre del circuito más corrido
 @app.get("/tres")
 def  tres():
-    miConexion = mysql.connector.connect( host="ace.com.pe",
-                                          user= 'acecompe_prueba1',
-                                          passwd='.&ve3Z5_VJW0', 
-                                          db="acecompe_prueba1"  )  
+
+    miConexion = mysql.connector.connect( host="estacion.educatics.org",
+                                          user= 'educaics_usr_est',
+                                          passwd='F5z!xZ5jhSyg', 
+                                          db="educaics_db_estacion"  )  
     cur = miConexion.cursor()
     cur.execute("select * from pregunta3")
     datos3 = [row for row in cur.fetchall()]
@@ -59,11 +59,10 @@ def  tres():
 #Piloto con mayor cantidad de puntos en total, cuyo constructor sea de nacionalidad sea American o British
 @app.get("/cuatro")
 def  cuatro():
-    
-    miConexion = mysql.connector.connect( host="ace.com.pe",
-                                          user= 'acecompe_prueba1',
-                                          passwd='.&ve3Z5_VJW0', 
-                                          db="acecompe_prueba1"  )  
+    miConexion = mysql.connector.connect( host="estacion.educatics.org",
+                                          user= 'educaics_usr_est',
+                                          passwd='F5z!xZ5jhSyg', 
+                                          db="educaics_db_estacion"  )  
     cur = miConexion.cursor()
     cur.execute("select  * from pregunta4" )
     datos4 = [row for row in cur.fetchall()]
